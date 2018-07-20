f690c52c6151725d69a2036dbe6c4304a1887e9b
Grecia <gutierrezanicamalucero@gmail.com>
2018-07-19 22:07:32 -0500

    subiendo demo2-vs1

--- src/main.js
+++ src/main.js
@@ -6,86 +6,62 @@ const loginButton = document.getElementById('loginButton');
 const formButton = document.getElementById('formButton');
 const formRegister = document.getElementById('formRegister');
 const formSesion = document.getElementById('formSesion');
-const errorEmail = document.getElementById('error-email');
-const errorEmail2 =  document.getElementById('error-email2');
-const errorPassword = document.getElementById('error-password');
-const errorPassword2 = document.getElementById('error-password2');
+let errorEmail = document.getElementById('error-email');
+let errorEmail2 = document.getElementById('error-email2');
+let errorPassword = document.getElementById('error-password');
+let errorPassword2 = document.getElementById('error-password2');
 const optionEmail = document.getElementById('optionEmail');
 const rButton = document.getElementById('optionR');
 const sButton = document.getElementById('optionS');
-let outButton = document.getElementById('signOut');
+const outButton = document.getElementById('signOut');
 const formTimeline = document.getElementById('formTimeline');
 // *******************************************************
-let email2 = document.getElementById('email2').value;
-let password2 = document.getElementById('password2').value;
-let nameUser = document.getElementById('nameUser').value;
-let email = document.getElementById('email').value;
-let password = document.getElementById('password').value;
-let confirPassword = document.getElementById('confirPassword').value;
+let email2 = document.getElementById('email2');
+let password2 = document.getElementById('password2');
+const cerrarSesion=document.getElementById('cerrar');
+let nameUser = document.getElementById('nameUser');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let confirPassword = document.getElementById('confirPassword');
 
+// cerrarSesion.addEventListener('click',eliminarUserBD);
+// const errorNameUser = document.getElementById('error-nameUser');
 
 // const userName = document.getElementById('user-name');
 // const userNameEmail = document.getElementById('user-email');
 // const userImage = document.getElementById('user-image');
 // const conectKey = "";
 // const cerrarSesion = document.getElementById('cerrar');
-// const errorNameUser = document.getElementById('error-nameUser');
 
-rButton.addEventListener('click', () => {
-  formRegister.classList.replace('hidden','show');
-  formButton.classList.replace('show','hidden');
-})
+rButton.addEventListener('click', event => {
+  event.preventDefault();
+  formRegister.classList.replace('hidden', 'show');
+  formButton.classList.replace('show', 'hidden');
+});
 
+// Boton Guardar datos de Usuario Registrado
 registerButton.addEventListener('click', () => { 
-  if (email == "") {
-    errorEmail.textContent = "Ingrese Correo";
-    alert('Debe ingresar su correo');
-
-  }else if (nameUser == ''){
-    errorNameUser.textContent = "Ingrese su nombre";
-    alert('Debe ingresar su nombre');
-
-  }else if (password == ''){
-    errorPassword.textContent = "IngresePassword";
-    alert('Debe ingresar su contraseña');
-
-  }else if (confirPassword == ''){
-    confirPassword.textContent = "Por favor confirmar Password";
-    alert('Debe confirmar su contraseña');
-
-  }else if (password != confirPassword){
-    alert('Las contraseñas no coinciden');  
-
-  } else if (errorEmail == 'incorrecto'){
-    alert('por favor ingrese un email válido'); 
-    
-  } else {
-    errorNameUser.textContent = "";
-    errorEmail.textContent = "";
-    errorPassword.textContent = "";
-    confirPassword.textContent = "";
-    let newsUser = registerNew(email, password);
-    alert('Tu usuario ha sido registrado! \nConfirma el mensaje de verificación en tu correo y seguidamente puedes Iniciar Sesión')
-    console.log(email);
-    console.log(password);
+  validationRegisterUser();
+  email = email.value;
+  password = password.value;
+  // validationEmailOK (email);
+  // if(errorEmail.value =='incorrecto'){
+  // alert('ingrese un email válido')
+  // }
+  registerNew(email, password);
     formRegister.style.display = 'none';
-    formButton.style.display = 'block';  
-  }  
+    formButton.style.display = 'block';
 });
 
 loginButton.addEventListener('click', () => {
+  
+  email2 = email2.value;
+  password2 = password2.value;
+  // validationLoginUser(email2, password2);
   let userLogin = login(email2, password2);
-
-  if (email2 == ''){
-    errorEmail2.textContent = "Ingrese su Correo";
-  }else if (password2 == ''){
-    errorPassword2.textContent = "Ingrese su Password";
-  }else if (email2 != '' && password2 != '') {
-    errorEmail2.textContent = "";
-    errorPassword2.textContent = "";
-    validation();
-  }   
+  validation();
 });
+
 
 googleButton.addEventListener('click', (e) => {
   if (e.target) {
@@ -99,11 +75,15 @@ faceButton.addEventListener('click', (e) => {
   }
 });
 
+// const outButton.addEventListener('click', () => {
+//   signOut();
+//   // window.location.href = 'index.html';
+//   formTimeline.style.display = 'none';
+//   formRegister.style.display = 'none';
+//   formButton.style.display = 'block';
+// });
+
 outButton.addEventListener('click', () => {
   signOut();
-  // window.location.href = 'index.html';
-  formTimeline.style.display = 'none';
-  formRegister.style.display = 'none';
-  formButton.style.display = 'block'; 
-});
-
+  window.location.href = 'index.html';
+})
